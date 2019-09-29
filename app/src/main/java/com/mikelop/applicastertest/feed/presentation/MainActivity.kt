05b0d3206dd34e534aeab119b9ce1273931afbec
Miguel Lopez <mike_salais@msn.com>
2019-09-28 21:27:18 -0500

    Configure the libraries needed for the proj.

---
+++ app/src/main/java/com/mikelop/applicastertest/feed/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mikelop.applicastertest.feed.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mikelop.applicastertest.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
