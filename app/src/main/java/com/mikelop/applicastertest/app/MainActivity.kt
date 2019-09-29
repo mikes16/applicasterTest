f30645b36d3d1f342e46fcd55eecef05fc6f9410
Miguel Lopez <mike_salais@msn.com>
2019-09-28 23:50:14 -0500

    App icon and colors added

--- app/src/main/java/com/mikelop/applicastertest/app/MainActivity.kt
+++ app/src/main/java/com/mikelop/applicastertest/app/MainActivity.kt
@@ -3,11 +3,14 @@ package com.mikelop.applicastertest.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mikelop.applicastertest.R
+import org.json.JSONArray
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme_NoActionBar)
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
