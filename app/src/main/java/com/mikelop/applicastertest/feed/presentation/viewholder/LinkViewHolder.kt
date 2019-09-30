116f390a75382e2ed7be089da802518b9c8caf80
Miguel Lopez <mike_salais@msn.com>
2019-09-29 19:44:01 -0500

    Finished link implementation

--- app/src/main/java/com/mikelop/applicastertest/feed/presentation/viewholder/LinkViewHolder.kt
+++ app/src/main/java/com/mikelop/applicastertest/feed/presentation/viewholder/LinkViewHolder.kt
@@ -7,16 +7,15 @@ import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.mikelop.applicastertest.R
+import com.mikelop.applicastertest.link.presentation.view.NewsLinkFragmentArgs
 import kotlinx.android.synthetic.main.item_feed_link.view.*
 
-internal class LinkViewHolder (view: View) : FeedParentViewHolder(view){
+internal class LinkViewHolder (val view: View) : FeedParentViewHolder(view){
     val titleTv: TextView = view.link_title_tv
     val summaryTv: TextView = view.link_summary_tv
     val imageIv: ImageView = view.link_news_iv
-    val contentSrc: String = ""
 
-    init {
-        val bundle = bundleOf("contentSrc" to contentSrc)
-        view.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.newsLinkFragment, bundle))
+    fun setClickListener(link: String = "", title: String = ""){
+        view.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.newsLinkFragment, NewsLinkFragmentArgs(link, title).toBundle()))
     }
 }
